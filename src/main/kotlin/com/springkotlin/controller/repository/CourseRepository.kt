1436c8a2a716e42d48eb7b6b47f440ef2950a758
pbs009 <prakash.sharma1@telekom-digital.com>
2023-11-28 10:36:56 +0530

    Add course entity and dto and integrate h2 databse

---
+++ src/main/kotlin/com/springkotlin/controller/repository/CourseRepository.kt
@@ -0,0 +1,7 @@
+package com.springkotlin.controller.repository
+
+import com.springkotlin.controller.entity.Course
+import org.springframework.data.repository.CrudRepository
+
+interface CourseRepository: CrudRepository<Course, Int> {
+}
